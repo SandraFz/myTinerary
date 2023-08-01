a56f104c034c0f3a508b8b047aaf57f5115ba190
Sandra <sandranfernandez@gmail.com>
2023-07-31 22:25:18 -0300

    Estructura de la página inicial.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../public/assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -8,26 +8,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li>My Itinerary</li>
+              <li>Home</li>
+              <li>Cities</li>
+              <li>Login</li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <div>
+            <div>
+              <h1>Find the perfect destination</h1>
+              <p>Our app will help you find the perfect path for your next trip. With an easy-to-use interface and host of itinerary options, planning your next trip has never been easier.</p>
+              <input type="submit" value="View more"/>
+            </div>
+            <div>
+              <img src="../public/assets/tourism-shutterstock_460303915-780.jpg" alt="Turista" />
+            </div>
+          </div>
+        </main>
+        <footer>
+
+        </footer>
+      </body>
     </>
   )
 }
