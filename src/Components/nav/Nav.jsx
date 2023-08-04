49a606f0c706ae5df8ef56951057fc91c64fc7c1
Sandra <sandranfernandez@gmail.com>
2023-08-04 12:49:34 -0300

    Nav responsive funcionando.

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -6,16 +6,16 @@ function Nav() {
     return (
       <>
       
-      <nav class="navbar navbar-dark navbar-expand-lg w-100">
+      <nav class="navbar navbar-dark navbar-expand-md w-100">
         <div className="container-fluid containerTittle">
           <a className="text-decoration-none tittle" href="#">
             My Itinerary
           </a>
-          <div className=" d-flex flex-row-reverse w-100">
+          <div className=" w-100">
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
            
             <img className="" src="public/assets/images/menu.png" alt="" />
-          
+            </button>
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 d-flex justify-content-end">
                   <li className="nav-item">
@@ -33,7 +33,7 @@ function Nav() {
                 </ul>
                 
               </div>
-            </button>
+            
           
           </div>
           
