bb6de8962765840accb3a6fcac449a023563d9d1
Sandra <sandranfernandez@gmail.com>
2023-08-03 22:04:57 -0300

    Nav desplagable funcionando. Se rompe en pantallas grandes.

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -6,29 +6,35 @@ function Nav() {
     return (
       <>
       
-      <nav class="navbar navbar-expand-lg w-100">
+      <nav class="navbar navbar-dark navbar-expand-lg w-100">
         <div className="container-fluid containerTittle">
-          <a className="text-decoration-none" href="#">
+          <a className="text-decoration-none tittle" href="#">
             My Itinerary
           </a>
-          <div className=" d-flex flex-row-reverse">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+          <div className=" d-flex flex-row-reverse w-100">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           
+            <img className="" src="public/assets/images/menu.png" alt="" />
           
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 d-flex">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Cities</a>
-              </li>
-            </ul>
-            
-          </div>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 d-flex justify-content-end">
+                  <li className="nav-item">
+                    <a href="" className="login">
+                      <img className="w-50" src="public/assets/images/perfil-white.png" alt="Login" />
+                      Login
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Cities</a>
+                  </li>
+                </ul>
+                
+              </div>
+            </button>
           
-          <input type="submit" value="login" />
           </div>
           
           
