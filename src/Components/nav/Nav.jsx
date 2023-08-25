7dd9969834b98f0155639227725fe57d791b676b
Sandra <sandranfernandez@gmail.com>
2023-08-25 19:17:39 -0300

    Nav con avatar.

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -20,25 +20,23 @@ const aList = ArrayList.navbar
                 <img src={logo} alt="" />
               </div>
                 
-              <div className="divLinks w-25">
+              <div className="divButtons w-25">
 
-              < button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <img className="menuItem" src={"/assets/images/menu.png"} alt="" />
-              </button>
+                <div className="avatar">
+                  <img src="/assets/images/perfil-white.png" alt="" />
+                </div>
+
+                < button className="navbar-toggler buttonMenu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <img className="menuItem" src={"/assets/images/menu.png"} alt="" />
+                </button>
               
-
               </div>  
 
             </div>
             <div className="buttonContainer">
                 <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex w-100">
-                    <AnchorList list={aList}>
-                      <Anchor to="#" className="button">
-                        <img className="" src="/assets/images/perfil-white.png" alt="Login" />
-                        Login
-                      </Anchor>
-                    </AnchorList>
+                    <AnchorList list={aList}/>
                   </ul>
                 </div>
               </div>
