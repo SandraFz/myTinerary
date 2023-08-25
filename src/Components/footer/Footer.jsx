eb5ae8c81cda7492d66e38e53c073495f1a0e206
Sandra <sandranfernandez@gmail.com>
2023-08-25 19:35:59 -0300

    Footer mejorado.

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Footer.css'
 import AnchorList from "../anchorList/AnchorList";
 import ArrayList from '../../assets/data/links.json'
+import { Link as Anchor } from "react-router-dom";
 
 function Footer() {
 
@@ -9,17 +10,15 @@ function Footer() {
 
     return (
         <footer className="d-flex justify-content-between align-items-center p-3 footer">
-            <div className="d-flex justify-content-between redes-group">
-                 <a href="#" className="mx-2 a-redes-logo d-inline-block">
-                    <img className="rounded-5" src="../../../../public/assets/images/instagram.jpg" alt="Logo instagram"/>
-                </a>
-                <a href="#" className="mx-2 a-redes-logo d-inline-block">
-                    <img className="rounded-5" src="../../../../public/assets/images/facebook.jpg" alt="Logo Facebook"/>
-                </a>
-                <a href="#" className="mx-2 a-redes-logo d-inline-block">
-                    <img className="rounded-5" src="../../../../public/assets/images/whatsapp.jpg" alt="Logo WhatsApp"/>
-                </a>
-            </div>
+            <ul className="d-flex justify-content-between redes-group footerMenu">
+                {
+                    aList.redes.map(elem => {
+                        return <Anchor href="#" className="m-1 a-redes-logo d-inline-block">
+                        <img className="rounded-5" src={elem.src} alt={elem.alt}/>
+                    </Anchor>
+                    })
+                }
+            </ul>
             <ul className="navbar-nav d-flex justify-content-between redes-group footerMenu">
                 <AnchorList list={aList.navbar}/>
             </ul>
