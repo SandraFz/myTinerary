f1d9cddff32ac431d9a9a9811ec86b631fe8f75e
Sandra <sandranfernandez@gmail.com>
2023-09-21 16:01:33 -0300

    Control de nav según roles funcionando.

--- src/Components/anchorList/AnchorList.jsx
+++ src/Components/anchorList/AnchorList.jsx
@@ -4,15 +4,24 @@ import { Link as Anchor } from "react-router-dom";
 
 const AnchorList = ({children, list}) => {
 
+    const status = localStorage.getItem('online')
+    console.log(status)
+
     return (
         <>
             {children}
             {
             list.map(elem => {
-                
-                return  <li className="nav-item m-1"  key={elem.value}>
+                if(status && elem.role.includes('logged')){
+                    return  <li className="nav-item m-1"  key={elem.value}>
                             <Anchor className="text" /* onClick={elem.fn}  */to={elem.to}>{elem.value}</Anchor>
                         </li>
+                } else if(!status && elem.role.includes('default')){
+                    return  <li className="nav-item m-1"  key={elem.value}>
+                            <Anchor className="text" /* onClick={elem.fn}  */to={elem.to}>{elem.value}</Anchor>
+                        </li>
+                }
+                
             })
             
             }
